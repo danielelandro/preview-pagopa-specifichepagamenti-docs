c5757bc7ce4ca1045f33571d0d4b5f2829d819e5
danielelandro <43603429+danielelandro@users.noreply.github.com>
2019-01-18 22:41:39 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -33,10 +33,6 @@ rtd_project = os.environ.get('READTHEDOCS_PROJECT', '')
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath('.')
 
 # -- General configuration -----------------------------------------------------
 
@@ -52,8 +48,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.ifconfig',
-    'docs-italia-theme',
-    'part-toctree'
+    'docs-italia-theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
